7438f2de8d9ed48ae396ba13e0d0d1dd2393aa2d
Manu Sharma <manusharmaqainfotech@gmail.com>
2018-02-27 18:23:51 +0530

    Update test cases

--- mochatest/app.js
+++ mochatest/app.js
@@ -1,3 +1,11 @@
-module.exports = function(){
+module.exports = {
+  abc1: function(){
   return 'Hello1';
+},
+abc2: function(){
+  return 566;
+},
+add: function(num1, num2){
+return num1+num2;
+}
 }
--- mochatest/test/aapTest.js
+++ mochatest/test/aapTest.js
@@ -1,7 +1,24 @@
 const assert = require('chai').assert;
-const app = require('../app');
+
+var app = require('../app');
+
 describe('App',function(){
   it('app should return hello ', function(){
-    assert.equal(app(),'Hello');
+    assert.equal(app.abc1(),'Hello1');
+  })
+  it('app should return hello ', function(){
+    assert.typeOf(app.abc1(),'String');
+  })
+  it('app should return hello ', function(){
+    assert.equal(app.abc2(),566);
+  })
+  it('app should return hello ', function(){
+    assert.typeOf(app.abc2(),'Number');
+  })
+  it('app should return hello ', function(){
+    assert.equal(app.add(7,9),16);
+  })
+  it('app should return hello ', function(){
+    assert.typeOf(app.add(7,9),'Number');
   })
 })
