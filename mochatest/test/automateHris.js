1c322e248f27e09288a24b6a209586434eb47537
Manu Sharma <manusharmaqainfotech@gmail.com>
2018-03-06 18:28:51 +0530

    implementing promise

--- mochatest/test/automateHris.js
+++ mochatest/test/automateHris.js
@@ -1,4 +1,4 @@
-
+var assert = require('assert');
  var webdriver = require('selenium-webdriver');
  var until = require('selenium-webdriver').until;
  var By = require('selenium-webdriver').By;
@@ -8,14 +8,16 @@ it("Login in to HRIS",function(){
   var driver = new webdriver.Builder().forBrowser('chrome').build();
     driver.get("http://hris.qainfotech.com/login.php",5000);
   driver.findElement(By.css('input[name="txtUserName"]')).sendKeys("manusharma");
-  driver.findElement(By.css('input[name="txtPassword"]')).sendKeys("xyxabc");
-  driver.findElement(By.css('input[name="Submit"]')).click();
-//  driver.wait(until.elementLocated(By.css('a>img[class="user-image"]+span')));
-  var username = driver.findElement(By.css('a>img[class="user-image"]+span'));
-  username.getText().then(function(text)
-  {
-    assert.equal(text,'Manu');
+  driver.findElement(By.css('input[name="txtPassword"]')).sendKeys("Manu@90153764");
+  driver.findElement(By.css('input[name="Submit"]')).click().then(function(){
+    var username = driver.findElement(By.css('a>img[class="user-image"]+span'));
+    username.getText().then(function(text)
+    {
+      assert.equal(text,'Manudd');
+    });
   });
+//  driver.wait(until.elementLocated(By.css('a>img[class="user-image"]+span')));
+
 });
 
 });
